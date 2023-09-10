186b0c7e2a438dde6693e54d716f0b7cdfa828f8
limxuankai <limxuankai3645@gmail.com>
2023-09-10 17:16:25 +0800

    dashboard added

--- cock/home/views.py
+++ cock/home/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def home(request):
+    print(request)
+    if request.method == 'POST':
+        username = request.POST.get('Name')
+        password = request.POST.get('Password')
+        print(user)
+        return redirect("dashboard")
     return render(request, "home.html")
+
+def dashboard(request):
+    return render(request, "dashboard.html")
+    
